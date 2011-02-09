7133686732555084abc58a9c7e419192e77adb58
Derek Kastner <dkastner@gmail.com>
2011-02-08 23:02:14 -0500

    Initialize css_includes

--- theme/include/partials/header.php
+++ theme/include/partials/header.php
@@ -1,6 +1,7 @@
 <? 
 $js_includes = isset($js_includes) ? $js_includes : array();
 $breadcrumbs = isset($breadcrumbs) ? $breadcrumbs : array('Home' => '');
+$css_includes = isset($css_includes) ? $css_includes : array();
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
