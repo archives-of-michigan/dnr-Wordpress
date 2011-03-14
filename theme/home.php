e4be6053fe9d7e47784d8913805faaeee25d2710
JillArnold <arnoldjil@gmail.com>
2011-03-14 17:09:35 -0400

    Removing thumbnail image for now

--- theme/home.php
+++ theme/home.php
@@ -28,7 +28,7 @@ app()->partial('header',
   <div id="discover-bar">
     <div class="wrapper">
       <h3>Wildlife Division Reports</h3>
-      <a href="http://cdm15559.contentdm.oclc.org/seeking_michigan/seek_results.php?CISOROOT=/p15559coll1" title="View Wildlife Division Reports"><img style="border: 1px #aaa solid; padding: 3px;" src="http://dnr.seekingmichigan.org/wp-content/uploads/2011/02/dnre-deer.jpg" alt="Photo for Wildlife Division Reports" /></a>
+      <!-- <a href="http://cdm15559.contentdm.oclc.org/seeking_michigan/seek_results.php?CISOROOT=/p15559coll1" title="View Wildlife Division Reports"><img style="border: 1px #aaa solid; padding: 3px;" src="http://dnr.seekingmichigan.org/wp-content/uploads/2011/02/dnre-deer.jpg" alt="Photo for Wildlife Division Reports" /></a> -->
       <h4><a href="http://cdm15559.contentdm.oclc.org/seeking_michigan/seek_results.php?CISOROOT=/p15559coll1" title="View Wildlife Division Reports">Reports from the Wildlife Division</a></h4>
       <p>A collection of surveys and other material from the Wildlife Division library.</p>
       <p><a href="/discover-collection?collection=p15559coll1" title="Read more">Read more »</a> | <a href="/discover" title="View more collections">View more collections &raquo;</a></p>
