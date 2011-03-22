5553fda5048bf048cd65ed95909d97216e14f7b8
JillArnold <arnoldjil@gmail.com>
2011-03-22 08:20:50 -0400

    Changed homepage photo, fixed spacing above banner on Seek and Discover

--- theme/home.php
+++ theme/home.php
@@ -31,7 +31,7 @@ app()->partial('header',
       <p><a href="http://cdm15559.contentdm.oclc.org/seeking_michigan/seek_results.php?CISOROOT=/p15559coll1" title="View Wildlife Division Reports"><img style="max-width: 190px;
 max-height: 133px;
 border: 1px #AAA solid;
-padding: 3px;" src="http://dnr.seekingmichigan.org/wp-content/uploads/2011/02/dnre-deer.jpg" alt="Photo for Wildlife Division Reports" /></a></p>
+padding: 3px;" src="http://dnr.seekingmichigan.org/wp-content/uploads/2011/02/dnr-marsh-big.jpg" alt="Photo for Wildlife Division Reports" /></a></p>
       <!-- <h4><a href="http://cdm15559.contentdm.oclc.org/seeking_michigan/seek_results.php?CISOROOT=/p15559coll1" title="View Wildlife Division Reports">Reports from the Wildlife Division</a></h4> -->
       <p>A collection of surveys and other material from the Wildlife Division library.</p>
       <p><a href="/discover-collection?collection=p15559coll1" title="Read more">Read more »</a> | <a href="/discover" title="View more collections">View more collections &raquo;</a></p>
